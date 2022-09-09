61afa106f43acd8385a22ef63e55929102bff384
loupbu <98724823+loupbu@users.noreply.github.com>
2022-09-09 05:13:37 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8081,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "cf91e499-c4a0-4702-8f98-45f0541fb8f2",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/baipo01"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
